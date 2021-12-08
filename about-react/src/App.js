08996014b826051a4c4bb164dd919ba50d402c02
tudiiii <tudy0207@gmail.com>
2021-12-08 11:39:49 +0900

    Docs: 리액트 기본 흐름 정리

--- about-react/src/App.js
+++ about-react/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ImportConponent from './Introductory/R003_ImportConponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Start React 200 !</h1>
+      <p>HTML 적용하기</p>
+      <ImportConponent></ImportConponent>
     </div>
   );
 }
--- about-react/src/index.js
+++ about-react/src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// 브라우저에 컴포넌트를 보여줌
+// ReactDOM.render(param1, param2)의 형태로 param2에 param1을 그려준다는 뜻
 ReactDOM.render(
+
+  // param 1 -화면에 보여줘야되는 렌더링할 컴포넌트
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+
+  // param2 - 컴포넌트를 어디다 그려줄지. index.gml 파일에서 id가 root인 엘리먼트를 찾아 뿌려줌
   document.getElementById('root')
 );
 
